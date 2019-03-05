fda094cd2b38089d6b387f29ede22d05c687fef6
thomas <thomasmholder@gmail.com>
2019-03-05 18:46:00 -0500

    Initial commit

---
+++ StressID.py
@@ -0,0 +1,30 @@
+import pronouncing
+
+
+class Word:
+
+    def __init__(self, content):
+        self.stresseslist = []
+        self.content = content
+
+        self.phoneslist = pronouncing.phones_for_word(content)
+
+        for phone in self.phoneslist:
+            self.stresseslist.append(pronouncing.stresses(phone))
+
+
+class Sentence:
+    contentlist = []
+    wordlist = []
+    content = ""
+
+    def __init__(self, content):
+        self.contentlist = content.split()
+
+        for self.word in self.contentlist:
+            self.wordlist.append(Word(self.word))
+
+
+x = Sentence("permit teenage mutant ninja turtles")
+for word in x.wordlist:
+    print(word.stresseslist)
