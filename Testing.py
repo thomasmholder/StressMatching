da39e842ef0e7a773953ed548508e34095b535d3
thomas <thomasmholder@gmail.com>
2019-03-05 21:50:51 -0500

    Restated testing because scratches are only saved locally

---
+++ Testing.py
@@ -0,0 +1,9 @@
+from StressID import *
+
+x = Sentence("Teenage Mutant Ninja Turtles")
+y = Sentence("Peter Peter Pumpkin Eater")
+
+print(x.meterlist)
+print(y.meterlist)
+
+print(comparemeter(x, y))
