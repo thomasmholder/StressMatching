3de106b5cda2b7fafbe9ac9e98697a34cbab07bd
thomas <thomasmholder@gmail.com>
2019-03-05 21:50:05 -0500

    Removed Testing in favor of a scratch

--- Testing.py
+++
@@ -1,9 +0,0 @@
-from StressID import *
-
-x = Sentence("Teenage Mutant Ninja Turtles")
-y = Sentence("Peter Peter Pumpkin Eater")
-
-print(x.meterlist)
-print(y.meterlist)
-
-print(comparemeter(x, y))
