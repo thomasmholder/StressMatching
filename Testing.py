54a9b229f51da558dfa319b6c2003e4f58139c2a
thomas <thomasmholder@gmail.com>
2019-03-05 22:14:57 -0500

    removed randommatchingmeter file

--- Testing.py
+++ Testing.py
@@ -1,7 +1,7 @@
 from StressID import *
 
 x = Sentence("Teenage Mutant Ninja Turtles")
-y = Sentence("Peter Peter Pumpkin Eater")
+y = Sentence("southmark's sovietologist linenger's usurping")
 
 print(x.meterlist)
 print(y.meterlist)
